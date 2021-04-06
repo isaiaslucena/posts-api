a91408d40bb3a53538c7634e518aab0119cb78f2
isaiasneto <isaiasneto@gmail.com>
2021-04-06 19:13:05 -0300

    Add controller to router

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getPost, createPost, updatePost } from '../controllers/post';
+import { getPost, createPost, updatePost, deletePost } from '../controllers/post';
 
 const router = Router();
 
 router.get('/', getPost);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
-router.delete('/:id');
+router.delete('/:id', deletePost);
 
 export default router;
