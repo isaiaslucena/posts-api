0815c8dd0d714d3e0f059445c4ad35ec6f662cee
isaiasneto <isaiasneto@gmail.com>
2021-04-06 18:10:45 -0300

    Created updatePost function

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -20,3 +20,36 @@ export const createPost: RequestHandler = (req, res, next) => {
 
   res.json({ message: "Post created!", post });
 };
+
+export const updatePost: RequestHandler = (req, res, next) => {
+  const idParam = +req.params.id;
+  const {
+    author,
+    authorEmail,
+    publishAt,
+    content
+  }: {
+    author: string;
+    authorEmail: string;
+    publishAt: string;
+    content: string
+  } = req.body;
+
+  const jsonData = JSON.parse(fs.readFileSync(jsonDataFile, 'utf8'));
+
+  const findPostIndex = jsonData.findIndex((post: { id: number }) => post.id === idParam);
+  if (findPostIndex < 0) res.status(404).json({ message: `Post ID ${idParam} not found!` });
+
+  jsonData[findPostIndex] = {
+    ...jsonData[findPostIndex],
+    author: author ? author : jsonData[findPostIndex].author,
+    authorEmail: authorEmail ? authorEmail : jsonData[findPostIndex].authorEmail,
+    publishAt: publishAt ? publishAt : jsonData[findPostIndex].publishAt,
+    updatedAt: new Date().toISOString(),
+    content: content ? content : jsonData[findPostIndex].content
+  };
+
+  fs.writeFileSync(jsonDataFile, JSON.stringify(jsonData), 'utf8');
+
+  res.json({ message: "Post updated!", post: jsonData[findPostIndex] });
+};
